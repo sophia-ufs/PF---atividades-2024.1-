6d01c62ebc0ccfcf9374bce805d7b6af22ea34c7
Sophia Damm <sophia.mardones@dcomp.ufs.br>
2024-09-12 18:05:45 -0300

    Update Funções de Alta ordem.js

--- Funções de Alta ordem.js
+++ Funções de Alta ordem.js
@@ -8,4 +8,4 @@ Todas as funções de alta ordem vistas em sala de aula estão liberadas para us
 const lista = [100, 80, 60, 120, 90]
 const somar = (acc, x) => acc + x
 const lista2 = lista.map((x) => lista.reduce(somar,0))
-console.log (lista2[0])
+console.log (lista2)
