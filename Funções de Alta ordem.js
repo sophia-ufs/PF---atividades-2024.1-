f9ccc1dc4d8d93d22c804f5df6df43b508f9f014
Sophia Damm <sophia.mardones@dcomp.ufs.br>
2024-09-12 09:55:44 -0300

    Update Funções de Alta ordem.js

--- Funções de Alta ordem.js
+++ Funções de Alta ordem.js
@@ -1,3 +1,9 @@
+/*
+Dada uma lista com 5 elementos, [100, 80, 60, 120, 90], devolva a soma de todos os elementos, 
+só que usando Map no resultado
+Todas as funções de alta ordem vistas em sala de aula estão liberadas para uso
+*/
+
 const lista = [100, 80, 60, 120, 90]
 const somar = (acc, x) => acc + x
 const lista2 = lista.map((x) => lista.reduce(somar,0))
