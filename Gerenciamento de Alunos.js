3b60f10907372a02c2ad92015489ac640b1e984b
Sophia Damm <sophia.mardones@dcomp.ufs.br>
2024-09-18 22:53:35 -0300

    Update Gerenciamento de Alunos.js

--- Gerenciamento de Alunos.js
+++ Gerenciamento de Alunos.js
@@ -1,10 +1,9 @@
-/*
-1. Definição de Registro: Crie um registro chamado aluno com os seguintes campos:
+//Equipe: Ana Beatriz Soares Dantas, Sophia Damm Zogaib Mardones, Vanessa Santos Oliveira.
+/* 1. Definição de Registro: Crie um registro chamado aluno com os seguintes campos:
 nome: Nome do aluno(a) (string)
 idade: Idade do aluno(a) (número inteiro)
 matricula: Matrícula do aluno(a) (número inteiro)
-curso: Código do curso do aluno(a) (string), por exemplo, EC, SI, CC, etc.
-*/
+curso: Código do curso do aluno(a) (string), por exemplo, EC, SI, CC, etc. */
 
 const aluno = (nome, idade, matricula, curso) => {
     return {
@@ -15,31 +14,24 @@ const aluno = (nome, idade, matricula, curso) => {
     }
 }
 
-/*
-2. Criação da Turma: Crie uma lista chamada turma que armazenará instâncias dos(as) alunos(as).
-*/
+// 2. Criação da Turma: Crie uma lista chamada turma que armazenará instâncias dos(as) alunos(as).
 const turma = Object.freeze([])
 
-/*
-3. Adição de Alunos(as): 
-Implemente uma função adicionarAluno(nome, idade, matricula, curso) que cria um novo registro aluno com os valores passados como argumentos e o adiciona à turma.
-*/
+/*3. Adição de Alunos(as): Implemente uma função adicionarAluno(nome, idade, matricula, curso) 
+que cria um novo registro aluno com os valores passados como argumentos e o adiciona à turma.*/
 
 const adicionarAluno = (turma) => (nome, idade, matricula, curso) => {
     return [...turma, aluno(nome, idade, matricula, curso)]
 }
 
-/*
-4. Listagem de Alunos(as): 
-Crie uma função listarAlunos() que percorre a turma e exibe os detalhes de cada aluno(a) (nome, idade, matricula e curso) no console.
-*/
+/*4. Listagem de Alunos(as): Crie uma função listarAlunos() que percorre a turma e 
+exibe os detalhes de cada aluno(a) (nome, idade, matricula e curso) no console.*/
 
 const listarAlunos = (turma) => console.log(turma)
 
-/*
-5. Busca por Curso: Implemente uma função buscarPorCurso(curso) que recebe um curso como argumento e 
-retorna uma lista contendo todos os alunos do catálogo que pertencem ao curso especificado.
-*/
+/*5. Busca por Curso: Implemente uma função buscarPorCurso(curso) que recebe um curso como argumento e 
+retorna uma lista contendo todos os alunos do catálogo que pertencem ao curso especificado.*/
+
 const buscaPorCurso = (turma) => (curso) => {
     return turma.filter((x) => x.curso == curso)
 }
@@ -78,12 +70,11 @@ const contar = (turma) => (acc, aluno) => {
 }
 
 const ContagemPorCurso = (turma) => {
-    return turma.reduce(contar(turma), Object.freeze([]))
+    return turma.reduce(contar(turma), [])
 }
 
-/*
-9. Edição de Alunos: Permita a edição dos detalhes de um(a) aluno(a), como nome, matricula, idade ou curso. 
-*/
+//9. Edição de Alunos: Permita a edição dos detalhes de um(a) aluno(a), como nome, matricula, idade ou curso. 
+
 const editarAluno = (turma) => (old_matricula) => (novosDados) => {
     return turma.map((x) => x.matricula == old_matricula? novosDados : x)
 }
